bacbaaffda1e24f5ba3fb32340ce361e2649e68c
jutoungjun <goldrlawndud@naver.com>
2019-05-15 17:33:08 +0900

    게시판 수정~

--- app/src/main/java/edu/android/hashtravel/DashboardFragment.java
+++ app/src/main/java/edu/android/hashtravel/DashboardFragment.java
@@ -46,18 +46,20 @@ public class DashboardFragment extends Fragment {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                ArrayAdapter<CharSequence> adpter;
-               if(position == 0) {
+               if(position == 1) {
                    adpter = ArrayAdapter.createFromResource(view.getContext(), R.array.Asia_Country, android.R.layout.simple_spinner_item);
-               } else if(position == 1) {
-                   adpter = ArrayAdapter.createFromResource(view.getContext(), R.array.Europe_Country, android.R.layout.simple_spinner_item);
                } else if(position == 2) {
-                   adpter = ArrayAdapter.createFromResource(view.getContext(), R.array.America_, android.R.layout.simple_spinner_item);
+                   adpter = ArrayAdapter.createFromResource(view.getContext(), R.array.Europe_Country, android.R.layout.simple_spinner_item);
                } else if(position == 3) {
-                   adpter = ArrayAdapter.createFromResource(view.getContext(), R.array.SouthAmerica_Country, android.R.layout.simple_spinner_item);
+                   adpter = ArrayAdapter.createFromResource(view.getContext(), R.array.America_, android.R.layout.simple_spinner_item);
                } else if(position == 4) {
+                   adpter = ArrayAdapter.createFromResource(view.getContext(), R.array.SouthAmerica_Country, android.R.layout.simple_spinner_item);
+               } else if(position == 5) {
                    adpter = ArrayAdapter.createFromResource(view.getContext(), R.array.Africa_Country, android.R.layout.simple_spinner_item);
-               } else {
+               } else if(position == 6){
                    adpter = ArrayAdapter.createFromResource(view.getContext(), R.array.Oceania_, android.R.layout.simple_spinner_item);
+               } else {
+                   adpter = ArrayAdapter.createFromResource(view.getContext(), R.array.empty, android.R.layout.simple_spinner_item);
                }
 
                adpter.setDropDownViewResource(android.R.layout.simple_spinner_item);
---
+++ app/src/main/java/edu/android/hashtravel/WriteBord.java
@@ -0,0 +1,13 @@
+package edu.android.hashtravel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WriteBord extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_write_bord);
+    }
+}
