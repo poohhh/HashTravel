ffc296fbd86b22d5481d602a255260bb99fe5bbe
poohhh <ny2322@gmail.com>
2019-05-17 16:28:14 +0900

    DetailDashBoardView 추가

---
+++ app/src/main/java/edu/android/hashtravel/DetailDashboardViewItem.java
@@ -0,0 +1,13 @@
+package edu.android.hashtravel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailDashboardViewItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_dashboard_view_item);
+    }
+}
